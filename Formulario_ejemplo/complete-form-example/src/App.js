4589c467257737c1082b679590ec5347c7601efd
GabrielRodriguez <argabrielrodriguez15@gmail.com>
2024-09-24 17:54:27 -0600

    :memo: se agrego un estado de arreglo para agregar usuario, es decir se creo un arreglo para guardar usuarios

--- Formulario_ejemplo/complete-form-example/src/App.js
+++ Formulario_ejemplo/complete-form-example/src/App.js
@@ -1,15 +1,32 @@
+import { useState } from 'react';
 import useFormulario from './hooks/useFormulario';
 import Input from './components/Input';
 import Card from './components/Card';
 import Container from './components/Container';
 import Button from './components/Button';
+
 const App = () => {
 
-  const [formulario, handleChange] = useFormulario({ name: '', lastname: '', email: '' });
+  //Creamos un estado para almacenar los datos del formulario
+  //formulario, handleChange, resetForm son los metodos, que se crean en el hook useFormulario
+  //name, lastname, email son los campos del formulario que llegan como parametro al hook useFormulario
+  const [formulario, handleChange, resetForm] = useFormulario({ name: '', lastname: '', email: '', });
 
-  console.log(formulario);
+  //Creamos un estado para almacenar los usuarios
+  //es decir los datos que se ingresen en el formulario se almacenaran en un array
+  const [usuarios, setUsuarios] = useState([]);
+
+  console.log(formulario, usuarios);
+  const sumit = (e) => {
+    //Este metodo se usa para evitar que la pagina se recargue
+    e.preventDefault();
+    setUsuarios([
+      ...usuarios, formulario
+    ])
+    resetForm();
+  }
   return (
-    <form>
+    <form onSubmit={sumit}>
       <Container>
         <Card>
           <div style={{ padding: 20 }}>
--- Formulario_ejemplo/complete-form-example/src/hooks/useFormulario.js
+++ Formulario_ejemplo/complete-form-example/src/hooks/useFormulario.js
@@ -2,13 +2,22 @@ import { useState } from 'react';
 
 //Formulario es el objeto que se va a modificar, es decir dinamico
 const useFormulario = (elements) => {
-    const [formulario, setFormulario] = useState(elements);
-      const handleChange = (e) => {
-        setFormulario({
-          ...formulario,
-          [e.target.name]: e.target.value
-        });
-      };
-        return [formulario, handleChange];
+  //elements es un objeto que se pasa como parametro al hook useFormulario
+  //elements es un objeto que tiene los campos del formulario
+  //elements = {name:'', lastname:'', email:''}
+  const [formulario, setFormulario] = useState(elements);
+  const handleChange = (e) => {
+    setFormulario({
+      ...formulario,
+      [e.target.name]: e.target.value
+    });
+  };
+  const resetForm = () => {
+    //seteamos el formulario con los valores iniciales
+    setFormulario(elements);
+  }
+
+  //retornamos el formulario, el metodo handleChange y el metodo resetForm
+  return [formulario, handleChange, resetForm];
 }
 export default useFormulario;
